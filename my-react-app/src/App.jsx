52720230224e927911133a104531316f86a31887
Ewata Efe Faith <ewataefe721@gmail.com>
2026-01-26 15:55:39 +0100

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+
+import Button from './Button'
+import ProfilePicture from './ProfilePicture'
+
+function App() {
+    
+    return(<ProfilePicture />)
+}
+
+
+export default App
---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,8 @@
+ function Button() {
+
+  const handleClick = (e) => e.target.textContent = "OUCH!😭";
+
+   return(<button onDoubleClick={(e) => handleClick(e)}>Click ME!!!!😒</button>)
+}
+
+ export default Button

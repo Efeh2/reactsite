adbf5833462315a71e4b2f3bd5c6155a7a86920d
Ewata Efe Faith <ewataefe721@gmail.com>
2026-01-30 21:13:53 +0100

    comment

--- my-react-app/src/ColorPicker.jsx
+++ my-react-app/src/ColorPicker.jsx
@@ -1,27 +1,27 @@
 
-import React, {useState} from "react";
+// import React, {useState} from "react";
 
-function ColorPicker() {
+// function ColorPicker() {
 
-    const [color, setColor] = useState("#deb887");
+//     const [color, setColor] = useState("#deb887");
 
-    function handleColorChange(event) {
-        setColor(event.target.value);
-    }
+//     function handleColorChange(event) {
+//         setColor(event.target.value);
+//     }
 
-    return(
-        <div className="color-picker-container">
-            <h1>Color Picker</h1>
-            <div className="color-display"
-            style={{backgroundColor: color}}>
-                <p>Selected Color: {color}</p>
-            </div>
+//     return(
+//         <div className="color-picker-container">
+//             <h1>Color Picker</h1>
+//             <div className="color-display"
+//             style={{backgroundColor: color}}>
+//                 <p>Selected Color: {color}</p>
+//             </div>
 
-            <label>Select a Color: </label>
-            <input type="color" value={color} 
-            onChange={handleColorChange} />
-        </div>
-    )
-}
+//             <label>Select a Color: </label>
+//             <input type="color" value={color} 
+//             onChange={handleColorChange} />
+//         </div>
+//     )
+// }
 
-export default ColorPicker
+// export default ColorPicker

0da74628664053b365863b4175808470161568ce
Ewata Efe Faith <ewataefe721@gmail.com>
2026-02-01 20:16:44 +0100

    .

--- my-react-app/src/CarUpdate.jsx
+++ my-react-app/src/CarUpdate.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 function CarUpdate(){
 
     const [cars, setcCars] = useState([]);
-    const [carYear, setCarYear] = useState([]);
+    const [carYear, setCarYear] = useState("");
 
     return(<div></div>);
 }

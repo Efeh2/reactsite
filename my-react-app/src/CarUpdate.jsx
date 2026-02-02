82f1c554fe2d702397964760042d0bb7a160431c
Ewata Efe Faith <ewataefe721@gmail.com>
2026-02-02 15:48:57 +0100

    handle

--- my-react-app/src/CarUpdate.jsx
+++ my-react-app/src/CarUpdate.jsx
@@ -3,8 +3,29 @@ import React, { useState } from "react";
 function CarUpdate(){
 
     const [cars, setcCars] = useState([]);
-    const [carYear, setCarYear] = useState("");
+    const [carYear, setCarYear] = useState(new Date().getFullYear());
+    const [carMake, setCarMake] = useState("");
+    const [carModel, setCarModel] = useState("");
 
+    function handleAddCar(){
+
+    }
+
+    function handleRemoveCar(index){
+
+    }
+
+    function handleYearChange(event){
+
+    }
+
+    function handleMakeChange(event){
+
+    }
+
+    function handleModelChange(event){
+
+    }
     return(<div></div>);
 }
 
